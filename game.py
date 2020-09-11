15faaca1c9c39edd735df98907b120e5d4d432cd
Eric Newman <newmaneric5@gmail.com>
2020-09-11 17:06:18 -0500

    Draw map from file

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 # Boilerplate
 import pygame as pg
 import sys
+from os import path
 from settings import *
 from sprites import *
 
@@ -14,8 +15,13 @@ class Game:
         self.load_data()
 
     def load_data(self):
-        pass
+        game_folder = path.dirname(__file__)
+        self.map_data = []
 
+        # open map.txt and read each line of it into map_data
+        with open(path.join(game_folder, 'map.txt'), 'rt') as f:
+            for line in f:
+                self.map_data.append(line)
 # Boilerplate end
 
     # initialize all variables and do all the setup for a new game
@@ -27,11 +33,16 @@ class Game:
         # create and render a player sprite
         self.player = Player(self, 10, 10)
 
+        for row, tiles in enumerate(self.map_data):
+            for col, tile in enumerate(tiles):
+                if tile == '1':
+                    Wall(self, col, row)
+
         # Room in top left corner
         # self.draw_room((0, 0), (6, 7))
-        # # hallway connecting left and right room
+        # hallway connecting left and right room
         # self.draw_room((6, 5), (10, 7))
-        # # room to the right, not very tall
+        # room to the right, not very tall
         # self.draw_room((10, 3), (22, 7))
 
     # game loop - set self.playing = False to end the game
@@ -48,34 +59,17 @@ class Game:
             # draw grid and sprites on to the screen
             self.draw()
 
-    def quit(self):
-        pg.quit()
-        sys.exit()
-
     def update(self):
         # calling update on a sprite group will call update on
         # all of the sprites within the group
         self.all_sprites.update()
 
-    def draw_grid(self):
-        for x in range(0, WIDTH, TILESIZE):
-            pg.draw.line(self.screen, LIGHTGREY, (x, 0), (x, HEIGHT))
-        for y in range(0, HEIGHT, TILESIZE):
-            pg.draw.line(self.screen, LIGHTGREY, (0, y), (WIDTH, y))
+    def quit(self):
+        pg.quit()
+        sys.exit()
 
-    def draw(self):
-        # screen is a surface, make it entirely one color
-        self.screen.fill(BGCOLOR)
-        self.draw_grid()
-        # this will call draw on each sprite in the group
-        # might be able to just draw the player?
-        self.all_sprites.draw(self.screen)
-
-        # Update the full display Surface to the screen
-        pg.display.flip()
-
+    # catch and handle all events here
     def events(self):
-        # catch all events here
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 self.quit()
@@ -100,8 +94,24 @@ class Game:
     def show_go_screen(self):
         pass
 
-# consider moving into a class Room
-# class Room:
+# drawing functions
+    def draw(self):
+        # screen is a surface, make it entirely one color
+        self.screen.fill(BGCOLOR)
+        self.draw_grid()
+        # this will call draw on each sprite in the group
+        # might be able to just draw the player?
+        self.all_sprites.draw(self.screen)
+
+        # Update the full display Surface to the screen
+        pg.display.flip()
+
+    def draw_grid(self):
+        for x in range(0, WIDTH, TILESIZE):
+            pg.draw.line(self.screen, LIGHTGREY, (x, 0), (x, HEIGHT))
+        for y in range(0, HEIGHT, TILESIZE):
+            pg.draw.line(self.screen, LIGHTGREY, (0, y), (WIDTH, y))
+
     def draw_room(self, start, end):
         start_x, start_y = start[0], start[1]
         end_x, end_y = end[0], end[1]
