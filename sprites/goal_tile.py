7592278900392b39d5a1f3f449ea643637a22626
Eric Newman <newmaneric5@gmail.com>
2024-09-04 22:21:29 -0500

    Images for Sprites
    - Add images for wall, goal tile, and player sprites
    - Create new diretory "images" to hold all the image files

--- sprites/goal_tile.py
+++ sprites/goal_tile.py
@@ -1,3 +1,5 @@
+from os import path
+
 import pygame as pg
 
 from settings import *
@@ -6,8 +8,10 @@ from settings import *
 class GoalTile(pg.sprite.Sprite):
     def __init__(self, x, y):
         pg.sprite.Sprite.__init__(self)
+        self.loaded_image = pg.image.load(path.join('images', 'treasure_chest.png')).convert_alpha()
         self.image = pg.Surface((TILESIZE, TILESIZE))
-        self.image.fill(WHITE)
+        self.image.blit(self.loaded_image, (0, 0))
+        pg.transform.smoothscale(self.loaded_image, self.image.get_size(), dest_surface=self.image)
         self.rect = self.image.get_rect()
         self.x = x
         self.y = y
--- sprites/player.py
+++ sprites/player.py
@@ -1,3 +1,5 @@
+from os import path
+
 import pygame as pg
 
 from settings import *
@@ -6,10 +8,12 @@ from settings import *
 class Player(pg.sprite.Sprite):
     def __init__(self, x, y):
         pg.sprite.Sprite.__init__(self)
+        self.loaded_image = pg.image.load(path.join('images', 'player.png')).convert_alpha()
         # Surface is pygame object for representing images
         # setting self.image and self.rect allows calls to pygame.Group.draw()
         self.image = pg.Surface((TILESIZE, TILESIZE))
-        self.image.fill(YELLOW)
+        self.image.blit(self.loaded_image, (0, 0))
+        pg.transform.smoothscale(self.loaded_image, self.image.get_size(), dest_surface=self.image)
         self.rect = self.image.get_rect()
         self.vx, self.vy = 0, 0
         self.x, self.y = x, y
